1c8f2604698e2f8292a39e2dbaea53d634b93f86
Shailesh L Prabhu <shaileshprabhu3@gmail.com>
2024-08-30 07:37:51 -0700

    Changed the epochs and trained and tested it

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-# loading the data
+# loading data
 mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 
